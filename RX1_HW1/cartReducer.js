7d01009732ad6514888f14376a67b84959c276ec
Priyanka <priyanka.ss1@outlook.com>
2024-11-14 22:46:02 +0530

    first commit

---
+++ RX1_HW1/cartReducer.js
@@ -0,0 +1,53 @@
+import { ADD_TO_CART, REMOVE_FROM_CART, CALCULATE_TOTAL } from './actions';
+const initialState = { cartItems: [], total: 0 };
+
+const cartReducer = (state = initialState, action) => {
+  //   console.log(state);
+  //spread the object to copy all properties of product
+  //find() => first time return undefined if item not found, findIndex() returns -1 means item not found
+  switch (action.type) {
+    // case "cart/added":
+    case ADD_TO_CART:
+      const existingItem = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      console.log(existingItem);
+
+      if (existingItem >= 0) {
+        const updatedQtyItem = state.cartItems.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+        return { ...state, cartItems: updatedQtyItem };
+      } else {
+        return {
+          ...state,
+          cartItems: [...state.cartItems, { ...action.payload, quantity: 1 }],
+        };
+      }
+
+    // case "cart/removed":
+    case REMOVE_FROM_CART:
+      return {
+        ...state,
+        cartItems: [
+          ...state.cartItems.filter((item) => item.id !== action.payload),
+        ],
+      };
+
+    // case "cart/calculateTotal":
+    case CALCULATE_TOTAL:
+      const total = state.cartItems.reduce(
+        (acc, curr) => acc + curr.price * curr.quantity,
+        0
+      );
+      return { ...state, total };
+
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
---
+++ RX1_HW1/main.js
@@ -0,0 +1,95 @@
+import cartReducer from './cartReducer';
+import { createStore } from 'redux';
+import { addToCart, removeFromCart, calculateTotal } from './actions';
+
+const store = createStore(
+  cartReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe(() => {
+  console.log(store.getState());
+  updateCart();
+});
+
+const products = [
+  { id: 1, name: 'Product A', price: 10 },
+  { id: 2, name: 'Product B', price: 20 },
+  { id: 3, name: 'Product C', price: 15 },
+];
+
+const productList = document.querySelector('#productList');
+const cartItemsList = document.querySelector('#cartItemsList');
+const total = document.querySelector('#total');
+
+window.addToCartHandler = (productId) => {
+  const product = products.find((product) => product.id === productId);
+  //   console.log(product);
+  //   store.dispatch({
+  //     type: "cart/added",
+  //     payload: product,
+  //   });
+  //   store.dispatch({ type: "cart/calculateTotal" });
+  store.dispatch(addToCart(product));
+  store.dispatch(calculateTotal());
+};
+
+window.removeFromCartHandler = (productId) => {
+  //   store.dispatch({ type: "cart/removed", payload: productId });
+  //   store.dispatch({ type: "cart/calculateTotal" });
+  store.dispatch(removeFromCart(productId));
+  store.dispatch(calculateTotal());
+};
+
+const renderProducts = () => {
+  productList.innerHTML = products
+    .map(
+      (product) =>
+        `<li>${product.name} - Rs. ${product.price} <button onClick="addToCartHandler(${product.id})">Add to cart</button></li>`
+    )
+    .join('');
+};
+
+renderProducts();
+
+const updateCart = () => {
+  const state = store.getState();
+  console.log(state);
+  cartItemsList.innerHTML = state.cartItems
+    .map(
+      (item) =>
+        `<li>${item.name} - Rs ${item.price} - Quantity: ${item.quantity} <button onClick = "removeFromCartHandler(${item.id})">Remove</button></li>`
+    )
+    .join('');
+
+  total.textContent = `Total: Rs ${state.total}`;
+};
+
+updateCart();
+
+// store.dispatch({
+//   type: "cart/added",
+//   payload: { id: 1, name: "Product A", price: 10 },
+// });
+// console.log(store.getState());
+
+// store.dispatch({
+//   type: "cart/added",
+//   payload: { id: 2, name: "Product B", price: 20 },
+// });
+// console.log(store.getState());
+
+// store.dispatch({
+//   type: "cart/added",
+//   payload: { id: 1, name: "Product A", price: 10 },
+// });
+// console.log(store.getState());
+
+// store.dispatch(addToCart({ id: 1, name: 'Product A', price: 10 }));
+// store.dispatch(calculateTotal());
+// store.dispatch(addToCart({ id: 2, name: 'Product B', price: 20 }));
+// store.dispatch(calculateTotal());
+// store.dispatch(addToCart({ id: 1, name: 'Product A', price: 10 }));
+// store.dispatch(calculateTotal());
+// store.dispatch(removeFromCart(1));
+// store.dispatch(calculateTotal());
